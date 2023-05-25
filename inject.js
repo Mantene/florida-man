c788d74e32437b6b646b19480b1a724c85d8c7f1
Matt Hood <mhood@princeton.edu>
2023-05-25 12:14:20 -0400

    add smart ’ as well

--- inject.js
+++ inject.js
@@ -6,6 +6,10 @@ function makeReplacements() {
 
 	const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, article, a, strong')
 	for (let i=0; i<text.length; i++) {
+        if (text[i].innerHTML.includes('Florida Governor DeSantis\’')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Florida Governor DeSantis\'', 'Florida Man\'s')
+        }
+
         if (text[i].innerHTML.includes('Florida Governor DeSantis\'')) {
             text[i].innerHTML = text[i].innerHTML.replace('Florida Governor DeSantis\'', 'Florida Man\'s')
         }
@@ -15,37 +19,61 @@ function makeReplacements() {
 		}
 
 
-        if (text[i].innerHTML.includes('Florida Governor Ron DeSantis\'')) {
+        if (text[i].innerHTML.includes('Florida Governor Ron DeSantis\’')) {
             text[i].innerHTML = text[i].innerHTML.replace('Florida Governor Ron DeSantis\'', 'Florida Man\'s')
 		}
 
+        if (text[i].innerHTML.includes('Florida Governor Ron DeSantis\'')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Florida Governor Ron DeSantis\'', 'Florida Man\'s')
+        }
+
         if (text[i].innerHTML.includes('Florida Governor Ron DeSantis')) {
             text[i].innerHTML = text[i].innerHTML.replace('Florida Governor Ron DeSantis', 'Florida Man')
+        }
+
+        if (text[i].innerHTML.includes('Governor DeSantis\'')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Governor DeSantis\'', 'Florida Man\'s')
+        }
+
+        if (text[i].innerHTML.includes('Governor DeSantis\’')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Governor DeSantis\’', 'Florida Man\'s')
         }
 
 		if (text[i].innerHTML.includes('Governor DeSantis')) {
 			text[i].innerHTML = text[i].innerHTML.replace('Governor DeSantis', 'Florida Man')
 		}
 
-        if (text[i].innerHTML.includes('Governor DeSantis\'')) {
-            text[i].innerHTML = text[i].innerHTML.replace('Governor DeSantis\'', 'Florida Man\'s')
+        if (text[i].innerHTML.includes('Governor Ron DeSantis\’')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Governor Ron DeSantis\’', 'Florida Man\'s')
+        }
+
+        if (text[i].innerHTML.includes('Governor Ron DeSantis\'')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Governor Ron DeSantis\'', 'Florida Man\'s')
         }
 
 		if (text[i].innerHTML.includes('Governor Ron DeSantis')) {
 			text[i].innerHTML = text[i].innerHTML.replace('Governor Ron DeSantis', 'Florida Man')
 		}
 
-		if (text[i].innerHTML.includes('Ron DeSantis\'')) {
-			text[i].innerHTML = text[i].innerHTML.replace('Ron DeSantis\'', 'Florida Man\'s')
+        if (text[i].innerHTML.includes('Ron DeSantis\’')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Ron DeSantis\’', 'Florida Man\'s')
 		}
+
+        if (text[i].innerHTML.includes('Ron DeSantis\'')) {
+            text[i].innerHTML = text[i].innerHTML.replace('Ron DeSantis\'', 'Florida Man\'s')
+        }
 
         if (text[i].innerHTML.includes('Ron DeSantis')) {
             text[i].innerHTML = text[i].innerHTML.replace('Ron DeSantis', 'Florida Man')
         }
 
-		if (text[i].innerHTML.includes('DeSantis\'')) {
+        if (text[i].innerHTML.includes('DeSantis\’')) {
 			text[i].innerHTML = text[i].innerHTML.replaceAll('DeSantis\'', 'Florida Man\'s')
 		}
+
+        if (text[i].innerHTML.includes('DeSantis\'')) {
+            text[i].innerHTML = text[i].innerHTML.replaceAll('DeSantis\’', 'Florida Man\'s')
+        }
 
         if (text[i].innerHTML.includes('DeSantis')) {
             text[i].innerHTML = text[i].innerHTML.replaceAll('DeSantis', 'Florida Man')
